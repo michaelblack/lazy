8f9e895409297afceea825803f18363f566f2f45
michaelblack <michael.bees@gmail.com>
2014-03-31 01:43:03 -0400

    removed streams and refactored the promise struct

--- lazy.c
+++ lazy.c
@@ -1,12 +1,10 @@
 #include <stdlib.h>
-#include "list.h"
 #include "lazy.h"
-#include "pairs.h"
 
 struct promise {
   char evaluated;
-  union data {
-    struct closurestruct {
+  union {
+    struct {
       void * (*function)(void ** args);
       void ** (args);
     } closure;
@@ -20,18 +18,19 @@ struct promise {
 */
 
 int evaluated(promise * p) {
-  return p->evaluated;
+  return (int)p->evaluated;
 }
 
-void * ( *promiseFunction(promise * p))(void **) {
+void * (*promiseFunction(promise * p))(void **) {
   return p->info.closure.function;
 }
+
 
 void ** promiseArgs(promise * p) {
   return p->info.closure.args;
 }
 
-void ** evaluation(promise * p) {
+void * evaluation(promise * p) {
   return p->info.evaluation;
 }
 
@@ -47,8 +46,9 @@ promise * promiseWrap(void * a) {
   return p;
 }
 
-promise * delay(void * (*function)(void ** args), void ** args ) {
+promise * delay(void * (*function)(void ** args), int args, ...) {
   promise * p = malloc(sizeof(promise));
+  
   p->info.closure.function = function;
   p->info.closure.args = args;
   p->evaluated = 0;
@@ -63,110 +63,3 @@ void * force(promise * p) {
   p->info.evaluation = (*(promiseFunction(p))) (promiseArgs(p));
   return evaluation(p);
 }
-
-/*
-  Streams
-*/
-
-struct stream {
-  void * head;
-  promise * tail;
-};
-
-stream * streamcons(void * head, promise * tail) {
-  stream * s = malloc(sizeof(stream));
-  s->head = head;
-  s->tail = tail;
-  return s;
-}
-
-void * head(stream * s) {
-  return s->head;
-}
-
-stream * tail(stream * s) {
-  return (stream *)force(s->tail);
-}
-
-
-list * take(int n, stream * s) {
-  list * head = listcons(NULL, NULL);
-  stream * straveler = s;
-  list * ltraveler = head;
-
-  if(n<=0)
-    return NULL;
-  
-  while(1) {
-
-    ltraveler->head = straveler->head;
-    straveler = (stream *) force(straveler->tail);
-
-    if (n == 1) {
-      break;
-    }    
-
-    ltraveler->tail = listcons(NULL,NULL);
-    ltraveler = ltraveler->tail;
-    n--;
-  }
-  
-  return head;
-}
-
-void * filterhelper(void ** args) {
-  return (void *) filter(args[0], args[1]);
-}
-
-stream * filter(int (*test)(void *), stream * s) {
-  if ((*test)(s->head)) {
-    void ** args = malloc(sizeof(void*)*2);
-    args[0] = test; args[1] = force(s->tail);
-    return streamcons(s->head, delay( &filterhelper, args));
-  }
-
-  return filter(test, (stream *) force(s->tail));
-}
-
-void * maphelper(void ** args) {
-  return (void *) map(args[0], args[1]);
-}
-
-stream * map(void * (*function)(void *), stream * s) {
-  void ** args = malloc(sizeof(void *) * 2);
-  args[0] = function; args[1] = (stream *) force(s->tail);
-  return streamcons((*function)(s->head), delay(&maphelper, args));
-}
-
-void * iteratehelper(void ** args) {
-  return (void *) iterate((void * (*)(void *))args[0], args[1]);
-}
-
-stream * iterate(void * (*f)(void *), void * x) {
-  void ** args = malloc(sizeof(void *) * 2);
-  args[0] = f; args[1] = (*f)(x);
-  return streamcons(x, delay(&iteratehelper, args));
-}
-
-void * zip2helper(void ** args) {
-  return zip2(args[0], args[1], args[2]);
-}
-
-stream * zip2(void * (*f)(void *, void *), stream * s1, stream * s2) {
-  void ** args = malloc(3 * sizeof(void *));
-  args[0] = f; args[1] = tail(s1); args[2] = tail(s2);
-  return streamcons( (*f)(head(s1), head(s2)), delay(&zip2helper, args));
-}
-
-
-void * unfoldhelper(void ** args) {
-  return unfold(args[0], args[1]);
-}
-
-stream * unfold(pair * (*f)(void *), void * acc) {
-  pair * p = (*f)(acc);
-  void ** args = malloc(2 * sizeof(void *));
-  args[0] = f; args[1] = second(p);
-  return streamcons( first(p), delay( &unfoldhelper, args));
-}
-  
--- lazy.h
+++ lazy.h
@@ -1,27 +1,13 @@
 #ifndef LAZY_H
 #define LAZY_H
-#include "list.h"
-#include "pairs.h"
 
 typedef struct promise promise;
-typedef struct stream stream;
 
 int evaluated(promise *);
 
 promise * promiseWrap(void *);
 promise * delay(void * (*)(void **), void **);
 void    * force(promise *);
-
-stream * streamcons(void *, promise *);
-void   * head(stream *);
-stream * tail(stream *);
-stream * filter(int (*)(void *), stream *);
-stream * map(void * (*)(void *), stream *);
-stream * iterate(void * (*)(void *), void *);
-stream * zip2(void * (*)(void *, void *), stream *, stream *);
-stream * unfold(pair * (*)(void *), void *);
-list * take(int, stream *);
-
 
 #endif
 
@@ -34,16 +20,6 @@ list * take(int, stream *);
   delay - takes a function and an array of args to later be passed to the function
   force - forces the function stored in the promise to be evaluated and returns the result
 
-  streamcons - Used for contructing a stream (an infinite list), takes a value and a promise for another stream
-  head - returns the head of the stream
-  tail - forces the tail of the stream and returns it
-  filter - takes a predicate p and filters out all elements of a stream for which p is not true
-  map - takes a function and applies it to each element of a stream
-  iterate - takes a function f and x builds a stream of x, f x, f f x, f f f x ...
-  zip2 - combines two streams into a single one by applying a function to their corresponding elements
-  unfold - keeps building a list by passing along an accumulator. Takes a function that takes the accumulator
-  and returns a pair of the new value to be in the list and the new accumulator
-
-  take - turns the first n elements of a stream into a list
+ 
 
 */
